eac75a67fe810d652d93cc0980d61e4d848b3cd2
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2022-06-12 21:36:08 +0200

    Feature modules with @NgModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,17 @@ import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, CommonModule],
+  imports: [
+    //angular modules
+    BrowserModule,
+    CommonModule,
+    //custom modules
+    PassengerDashboardModule,
+  ],
 })
 export class AppModule {}
