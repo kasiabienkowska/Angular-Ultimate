9c6fdfa5d2b844ca093a1716cb95ba3df7350091
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2022-06-12 20:41:41 +0200

    Pipes for data transformation

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ interface Passenger {
   id: number;
   fullname: string;
   checkedIn: boolean;
+  checkInDate?: number | null;
 }
 
 @Component({
@@ -17,47 +18,15 @@ interface Passenger {
         <li *ngFor="let passenger of passengers; let i = index">
           <span class="status" [class.checked-in]="passenger.checkedIn"></span>
           {{ i }}:{{ passenger.fullname }}
-        </li>
-      </ul>
-
-      <h3>Airline Passengers</h3>
-
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [ngClass]="{
-              'checked-in': passenger.checkedIn,
-              'checked-out': !passenger.checkedIn
-            }"
-          ></span>
-          {{ i }}:{{ passenger.fullname }}
-        </li>
-      </ul>
-
-      <h3>Airline Passengers</h3>
-
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [style.backgroundColor]="passenger.checkedIn ? 'green' : 'red'"
-          ></span>
-          {{ i }}:{{ passenger.fullname }}
-        </li>
-      </ul>
-
-      <h3>Airline Passengers</h3>
-
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [ngStyle]="{
-              backgroundColor: passenger.checkedIn ? 'green' : 'red'
-            }"
-          ></span>
-          {{ i }}:{{ passenger.fullname }}
+          <p>{{ passenger | json }}</p>
+          <div class="date">
+            Check in date:
+            {{
+              passenger.checkInDate
+                ? (passenger.checkInDate | date: "yMMMMd" | uppercase)
+                : "Not checked in"
+            }}
+          </div>
         </li>
       </ul>
     </div>
@@ -69,26 +38,31 @@ export class AppComponent {
       id: 1,
       fullname: "Stephen",
       checkedIn: true,
+      checkInDate: 1490742000000,
     },
     {
       id: 2,
       fullname: "Rose",
       checkedIn: false,
+      checkInDate: null,
     },
     {
       id: 3,
       fullname: "James",
       checkedIn: true,
+      checkInDate: 1491606000000,
     },
     {
       id: 4,
       fullname: "Louise",
       checkedIn: true,
+      checkInDate: 1488412800000,
     },
     {
       id: 5,
       fullname: "Tina",
       checkedIn: false,
+      checkInDate: null,
     },
   ];
 }
