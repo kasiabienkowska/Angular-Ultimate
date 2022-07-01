abf166dd1ebabcb2124cd694031fa9b622e6a208
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2022-07-02 00:57:05 +0200

    Observable.throw error handling

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -2,7 +2,9 @@ import { Passenger } from "./models/passenger.interface";
 import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/toPromise";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import "rxjs/add/observable/throw";
 
 const PASSENGER_API: string = "/api/passengers";
 
@@ -10,14 +12,14 @@ const PASSENGER_API: string = "/api/passengers";
 export class PassengerDashboardService {
   constructor(private http: Http) {}
 
-  getPassengers(): Promise<Passenger[]> {
+  getPassengers(): Observable<Passenger[]> {
     return this.http
       .get(PASSENGER_API)
-      .toPromise()
-      .then((response: Response) => response.json());
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
   }
 
-  updatePassengers(passenger: Passenger): Promise<Passenger> {
+  updatePassengers(passenger: Passenger): Observable<Passenger> {
     let headers = new Headers({
       "Content-Type": "application/json",
     });
@@ -26,14 +28,14 @@ export class PassengerDashboardService {
     });
     return this.http
       .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
-      .toPromise()
-      .then((response: Response) => response.json());
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
   }
 
-  removePassengers(passenger: Passenger): Promise<Passenger> {
+  removePassengers(passenger: Passenger): Observable<Passenger> {
     return this.http
       .delete(`${PASSENGER_API}/${passenger.id}`)
-      .toPromise()
-      .then((response: Response) => response.json());
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
   }
 }
